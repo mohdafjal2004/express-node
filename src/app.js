f03f5b33be7b359b3e18505a27d838bb1d9ac428
Mohd Afjal <mohdafjalgci2018@gmail.com>
2025-01-06 22:58:41 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -4,26 +4,6 @@ const express = require("express");
 //so this application is a new web server, so this server can listen
 //to incoming request
 const app = express(); //app = application
+app.get("/he*llo", (req, res) => res.send({ name: "Afjal" }));
 
-app.use("/dashboard/123", (req, res) => {
-  res.send("Hello from the dashboard 123");
-});
-app.use("/dashboard", (req, res) => {
-  res.send("Hello from the dashboard");
-});
-app.use("/test", (req, res) => {
-  res.send("Hello from the slash");
-});
-
-//this will only handle GET  calls to "/user"
-app.get("/user", (req, res) => {
-  res.send({ name: "Afjal", age: 100 });
-});
-//this will only handle POST  calls to "/user"
-app.post("/user", (req, res) => {
-  res.send("Data saved successfully");
-});
-app.use("/", (req, res) => {
-  res.send("Hello fr?om the asd");
-});
 app.listen(3000, () => console.log("Server is listening on port 3000...."));
