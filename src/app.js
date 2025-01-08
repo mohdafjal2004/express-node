7e9103631a0d74c7cd452cef8f8f0a8c9d01a3c8
Mohd Afjal <mohdafjalgci2018@gmail.com>
2025-01-08 07:32:19 +0530

    Another chaning the controller for the same route

--- src/app.js
+++ src/app.js
@@ -6,31 +6,15 @@ const express = require("express");
 const app = express(); //app = application
 app.get("/he*llo", (req, res) => res.send({ name: "Afjal" }));
 
-app.get(
-  "/user",
-  [
-    (req, res, next) => {
-      console.log("Handling the user route");
-      next();
-    },
-    (req, res, next) => {
-      console.log("Handling the  2nd user route");
+app.get("/user", (req, res, next) => {
+  console.log("Handling the 1st user route");
+  
+  next();
+});
+app.get("/user", (req, res) => {
+  console.log("Handling the 2nd user route");
 
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log("Handling the  𝟯𝗿d user route");
-
-    // res.send("𝟯𝗿𝗱 Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the  𝟰𝘁𝗵 user route");
-
-    res.send("𝟰𝘁𝗵 Response");
-    next();
-  }
-);
+  res.send("2nd Response");
+});
 
 app.listen(3000, () => console.log("Server is listening on port 3000...."));
