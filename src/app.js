1197727c77b88e8b478341ba7de1170a55b96e16
Mohd Afjal <mohdafjalgci2018@gmail.com>
2025-01-09 06:37:43 +0530

    Error handling using app.use

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 const express = require("express");
-const { authAdmin, userAuth } = require("./middlewares/authAdmin");
 
 const app = express();
 
-//handle Auth Middleware for all request GET, POST using app.use()
-app.use("/admin", authAdmin);
-app.get("/admin/fetchAllData", (req, res) => {
-  //logic for sending the data to the admin
-  res.send("All Data Sent");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  //logic for delete the user
-  res.send("Deleted a user");
-});
+
 
 // Another way of passing middleware
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User Data Sent");
+app.get("/user/data", (req, res) => {
+  try {
+    throw new Error("THis is error generated");
+  } catch (error) {
+    res.status(500).send("Some Error contact support team");
+  }
 });
+
+//This middleware is only called "After" the error is not handled in trycatch
+// in controlller of route of "/user/data"
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    //log your error and also handle the errror
+    res.status(500).send("Something went wrong");
+  }
+});
+
 
 app.listen(3000, () => console.log("Server is listening on port 3000...."));
