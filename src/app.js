e8c75b4faeae94aa7bf81923ceb5204c6ec7ea38
Mohd Afjal <mohdafjalgci2018@gmail.com>
2025-01-09 22:56:11 +0530

    User SIgn up dynamic data in database after connecting it to DB

--- src/app.js
+++ src/app.js
@@ -1,26 +1,22 @@
 const express = require("express");
-
+const connectDB = require("./config/database.js");
+const UserModel = require("./models/user.js");
 const app = express();
 
+app.use(express.json());
+app.post("/signup", async (req, res) => {
+  console.log(req.body);
 
-
-// Another way of passing middleware
-app.get("/user/data", (req, res) => {
-  try {
-    throw new Error("THis is error generated");
-  } catch (error) {
-    res.status(500).send("Some Error contact support team");
-  }
+  //Creating a new instance of the user model
+  const user = new UserModel(req.body);
+  await user.save();
+  res.send({ status: "User Saved Successfully", data: user });
 });
-
-//This middleware is only called "After" the error is not handled in trycatch
-// in controlller of route of "/user/data"
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    //log your error and also handle the errror
-    res.status(500).send("Something went wrong");
-  }
-});
-
-
-app.listen(3000, () => console.log("Server is listening on port 3000...."));
+connectDB()
+  .then(() => {
+    console.log("Datbase connection established...");
+    app.listen(3000, () => console.log("Server is listening on port 3000...."));
+  })
+  .catch((err) => {
+    console.error("Database connection cannot be established");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://mohdafjalgci2018:mohdafjalgci2018@learnnamastenodedemo.8srks.mongodb.net/devTinder"
+  );
+  //Remember here we directly edit in db string at the end due to which
+  //we have another database in the cluster
+};
+
+module.exports = connectDB
+
+
+
+  
