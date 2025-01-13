f615a767a574757d58b90095c518aaafe85bb4e1
Mohd Afjal <mohdafjalgci2018@gmail.com>
2025-01-13 07:39:23 +0530

    findById method

--- src/app.js
+++ src/app.js
@@ -4,39 +4,13 @@ const UserModel = require("./models/user.js");
 const app = express();
 
 app.use(express.json());
-app.post("/signup", async (req, res) => {
-  console.log(req.body);
 
-  //Creating a new instance of the user model
-  const user = new UserModel(req.body);
-  await user.save();
-  res.send({ status: "User Saved Successfully", data: user });
-});
-
-// GET user by email
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.email;
-  console.log("Email is ", userEmail);
-  try {
-    const user = await UserModel.find({ email: userEmail });
-    if (user.length > 0) {
-      res.send(user);
-    } else {
-      res.status(404).send("User not found");
-    }
-  } catch (error) {
-    res.status(500).send("Somethi ng went wrong");
-  }
-});
 // GET all USERS
 app.get("/all", async (req, res) => {
   try {
-    const user = await UserModel.find({});
-    if (user.length > 0) {
-      res.send(user);
-    } else {
-      res.status(404).send("User not found");
-    }
+    const user = await UserModel.findById("6780060f853ec9621e948220");
+
+    res.status(200).send({ user });
   } catch (error) {
     res.status(500).send("Somethi ng went wrong");
   }
